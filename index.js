780e6d4a949ed45c43f8560f4d6063916cb090d0
Great35 <Greatitodo35@gmail.com>
2025-04-10 16:39:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(PORT, () => {
 
 // Ping URLs
 const PING_URLS = [
-    "https://lemon-16bot.onrender.com", // Your main bot URL
+    "https://l16last.onrender.com/", // Your main bot URL
     "https://ping-xzu3.onrender.com" // Your second bot URL
 ];
 
