36800a2f98ec963785e97eefe399d0dd44bddaf4
Great35 <Greatitodo35@gmail.com>
2025-04-14 07:03:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ app.listen(PORT, () => {
 // Ping URLs
 const PING_URLS = [
     "https://l16last.onrender.com/", // Your main bot URL
-    "https://ping-xzu3.onrender.com" // Your second bot URL
+    "https://ping-xzu3.onrender.com"
+    "https://l16pay.onrender.com"
+    // Your second bot URL
 ];
 
 const INTERVAL = 3 * 60 * 1000; // Ping every 3 minutes
